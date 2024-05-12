b436b610fc655d8c616057b927e289cbb2afd940
mohd danish <mddanishyusuf@gmail.com>
2024-05-12 17:40:46 +0530

    s3 bucket route added

--- app/clients/s3.js
+++ app/clients/s3.js
@@ -1,11 +1,11 @@
 const AWS = require('aws-sdk');
 
-const spacesEndpoint = new AWS.Endpoint('sgp1.digitaloceanspaces.com');
+const spacesEndpoint = new AWS.Endpoint(process.env.SPACES_ENDPOINT);
 
 const s3 = new AWS.S3({
-      endpoint: spacesEndpoint,
-      accessKeyId: process.env.SPACES_KEY,
-      secretAccessKey: process.env.SPACES_SECRET,
+    endpoint: spacesEndpoint,
+    accessKeyId: process.env.SPACES_KEY,
+    secretAccessKey: process.env.SPACES_SECRET,
 });
 
 module.exports = s3;
